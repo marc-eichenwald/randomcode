4060440bb4c8f5b07d7674b3d11f1fae88738394
Marc Eichenwald <marceichenwald@protonmail.com>
2021-06-14 16:35:32 -0400

    minor change to a register store, fixed whitespace a bit (but it still is bad)

--- fizzbuzz/fizzbuzz.s
+++ fizzbuzz/fizzbuzz.s
@@ -11,7 +11,7 @@ _start:	push rbp
 	lea rsp, [rsp-24]
 	xor rcx, rcx ;; i stored here
 loop1:	lea rcx, [rcx+1] ;; i++
-	mov QWORD [rsp+24], rcx
+	mov QWORD [rbp], rcx
 	lea rax, [rcx] ;; loads i for the divide
 	xor rdx, rdx
 	lea rdi, [3]
@@ -20,33 +20,33 @@ loop1:	lea rcx, [rcx+1] ;; i++
 	jnz buzzcheck
 	lea rax, [rcx]
 	xor rdx, rdx
-	    lea rdi, [5]
-	    div rdi
-	    cmp rdx, 0 ;; mod 5 check
-	    jnz printfizz ;; if it passed the mod 3 but fails the mod 5 you can jump straight to printing fizz
-	   lea	 rdi, [rel fizzbuzz] ;; print fizzbuzz
-	    xor rax, rax
-	    call printf
-	    jmp loopend
-	printfizz:	lea rdi, [rel fizz]
-	    xor rax, rax
-	    call printf
-	    jmp loopend
-buzzcheck:	lea rax, [rcx]
-	    xor rdx, rdx
-	    lea rdi, [5]
-	    div rdi
-	    cmp rdx, 0
-	    jnz nope
-	    lea rdi, [rel buzz]
-	    xor rax, rax
-	    call printf
-	    jmp loopend
-	nope:		lea rdi, [rel nofizzbuzz]
-	    lea rsi, [rcx]
-	    xor rax, rax
+	lea rdi, [5]
+	div rdi
+	cmp rdx, 0 ;; mod 5 check
+	jnz printfizz ;; if it passed the mod 3 but fails the mod 5 you can jump straight to printing fizz
+	lea	 rdi, [rel fizzbuzz] ;; print fizzbuzz
+	xor rax, rax
 	call printf
-loopend:mov rcx, QWORD[rsp+24]
+	jmp loopend
+	printfizz:	lea rdi, [rel fizz]
+	xor rax, rax
+	call printf
+	jmp loopend
+buzzcheck:	lea rax, [rcx]
+	xor rdx, rdx
+	lea rdi, [5]
+	div rdi
+	cmp rdx, 0
+	jnz nope
+	lea rdi, [rel buzz]
+	xor rax, rax
+	call printf
+	jmp loopend
+nope:lea rdi, [rel nofizzbuzz]
+	lea rsi, [rcx]
+	xor rax, rax
+	call printf
+loopend:mov rcx, QWORD[rbp]
 	cmp rcx, 100
 	jne loop1
 	lea rsp, [rsp+24]
