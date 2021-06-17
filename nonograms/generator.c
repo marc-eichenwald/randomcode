a1f5320021cf29b5b9bec9c3f39ef7336d0cfdee
Marc Eichenwald <marceichenwald@protonmail.com>
2021-06-17 16:21:11 -0400

    refactored and finished generator function/variables last steps are program I/O

--- nonograms/generator.c
+++ nonograms/generator.c
@@ -5,32 +5,65 @@
 /*TODO: some code to read in png's or some sort of file
 */
 typedef struct nonogram{
-    int **rows;
-    int **cols;
+    rcstruct cols;
+    rcstruct rows;
 }nonogram;
 
-nonogram generate_nonogram(int **picture, int max_row, int max_col){
-    nonogram non;
-    non.rows = malloc(max_row * sizeof(int*));
-    non.cols = malloc(max_col * sizeof(int*));
-    int *current;
+typedef struct rcstruct{
+    int **rc;
+    int *count;
+}rcstruct;
+
+void malloc_check(void *p){
+    if (p=NULL){
+        err(EXIT_FAILURE, "failed to allocate a pointer");
+    }
+    return;
+}
+
+nonogram *generate_nonogram(int **picture, int max_row, int max_col){
+    nonogram *non;
+    int* current;
     int i,j,k,l;
     int dirty;
+    int count;
+    
+    non = malloc(sizeof(nonogram));
+    malloc_check(non);
+
+
+    non->rows.rc = malloc(max_row * sizeof(int*));
+    malloc_check(non->rows.rc);
+
+    non->cols.rc = malloc(max_col * sizeof(int*));
+    malloc_check(non->cols.rc);
+    non->cols.count = malloc(max_col * sizeof(int));
+    malloc_check(non->cols.count);
+    for (i=0; i<max_row; i++){
+        non->rows.rc[i] = malloc(sizeof(int) * ((max_col + 1) / 2)); //if odd makes n+1/2 if even just makes n/2
+        malloc_check(non->rows.rc[i]);
+    }
+    for (i = 0; i < max_col; i++){
+        non->cols.rc[i] = malloc(sizeof(int) * ((max_row + 1) / 2));
+        malloc_check(non->rows.rc[i]);
+    }
+
+
+    
     //row generation
-    for(i=0; i<max_row; i++){
-        current = picture[i];
-        k=0;
-        non.rows[i] = malloc(sizeof(int));
-        for (j=0; j<max_col; j++){
-            l=0;
-            if (current[j] == WHITE){
+    for(i = 0; i < max_row; i++){
+        dirty = 0;
+        k = 0;
+        for (j = 0; j < max_col; j++){
+            non->rows.count[i] = 0;
+            if (picture[i][j] == WHITE){
                 if(k == 0){
                     continue;
                 }
                 else{
-                    non.rows[i][l] = k;
+                    non->rows.rc[i][non->rows.count[i]] = k;
                     k = 0;
-                    non.rows[i] = realloc(non.rows[i], sizeof(int)*(++l));
+                    non->rows.count[i]++;
                 }
             }
             else{
@@ -39,21 +72,50 @@ nonogram generate_nonogram(int **picture, int max_row, int max_col){
             }
         }
         if (dirty == 0){
-            non.rows[i][l]=0;
+            *(non->rows.rc[i]) = 0;
         }
-
+        non->rows.count[i]++;
     }
 
-
+    //column generation;
+    for (i = 0; i < max_col; i++){
+        dirty = 0;
+        k = 0;
+        for (j = 0; j < max_col; j++){
+            non->cols.count[i] = 0;
+            if (picture[j][i] == WHITE){
+                if(k == 0){
+                    continue;
+                }
+                else{
+                    non->cols.rc[i][non->cols.count[i]] = k;
+                    k = 0;
+                    non->cols.count[i]++;
+                }
+            }
+            else{
+                k++;
+                dirty++;
+            }
+        }
+        if (dirty == 0){
+            *(non->cols.rc[i]) = 0;
+        }
+        non->cols.count[i]++;
+    }
 return non;
+}
+
+write_file(nonogram *non){
+    
 }
 
 int main(int argc, char *argv[]){
     int i,j,k;
     int **picture;
     int max_row, max_col;
-    FILE *output;
-    nonogram noni;
+    FILE *input, *output;
+    nonogram *non;
 
     //arg check
     if (argc != 3){
@@ -64,9 +126,9 @@ int main(int argc, char *argv[]){
     //read the file in here
 
 
-    generate_nonogram(picture, max_row, max_col);
+    non = generate_nonogram(picture, max_row, max_col);
 
-    write_file(noni);
+    write_file(non);
     
     return 0;
 }
