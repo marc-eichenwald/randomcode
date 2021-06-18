8e7e357f5dff9831cbcbe18b0cc77d105c65e1b9
Marc Eichenwald <marceichenwald@protonmail.com>
2021-06-17 23:30:09 -0400

    applied KISS principles to the build script

--- fizzbuzz/build.sh
+++ fizzbuzz/build.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
 #just a simple build and link of the input files
-bflag=
-cflag=
-while getopts c:b flag
-do
-    case $flag in
-	  b) bflag = true;;
-	  c) cflag = true;;
-      esac
-done
-if [(bflag && cflag) || (!bflag && !cflag)]; then
-   printf "Usage: %s [FLAG]\n" $0
-   printf "-b\t build\n"
-   printf "-c\t clean\n"
-   printf "select one and only one flag\n"
-   exit 2
-fi
-if [bflag]; then
-    nasm -f LIB64 fizzbuzz.s
-    ld fizzbuzz.o -lc -I/lib/ld-linux-x86-64.so.2 -o fizzbuzz.exe
-    rm fizzbuzz.o
-    exit 0
-fi
-if [cflag]; then
-    rm fizzbuzz.exe
-    exit 0
-fi
+nasm fizzbuzz.s -L ELF64 
+ld fizzbuzz.o -lc -I/lib/ld-linux-x86-64.so.2 -o fizzbuzz.exe
+rm fizzbuzz.o
